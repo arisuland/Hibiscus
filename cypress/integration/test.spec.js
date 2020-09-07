43794c0041f7aa6e490424f9c28fa94521753cde
August <august@augu.dev>
2020-09-06 23:38:29 -0700

    Implement e2e testing with Cypress

---
+++ cypress/integration/test.spec.js
@@ -0,0 +1,8 @@
+/// <reference types='cypress' />
+
+describe('monori.TestSpec', () => {
+  it('should say "Sup" when redirected to the Homepage', () => {
+    cy.visit('/');
+    cy.get('h1').contains('Sup!');
+  });
+});
