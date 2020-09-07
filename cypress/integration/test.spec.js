4cabe6445ea91557769076e7b3d308f7967fe39c
August <august@augu.dev>
2020-09-07 02:41:49 -0700

    Fix e2e testing

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,8 +1,8 @@
 /// <reference types='cypress' />
 
 describe('monori.TestSpec', () => {
-  it('should say "Sup" when redirected to the Homepage', () => {
+  it('should say "Hello" when redirected to the Homepage', () => {
     cy.visit('/');
-    cy.get('h1').contains('ea sports');
+    cy.get('h1').contains('Hello!');
   });
 });
